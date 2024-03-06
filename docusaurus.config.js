336199053b2dd26e2660ddb2f819f3e4c41f77cd
andreiusq <mrandyadv@gmail.com>
2024-03-06 12:21:42 +0200

    Modified config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'docs', // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
